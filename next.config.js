32260f2de477d919b0997fa085ed7e68e7499ad6
DrParticlePhysicist <ritikawasthi55@gmail.com>
2025-01-28 20:35:41 +0530

    added nojerkyll to places and modified next.config,js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  // If deploying to GitHub Pages with a repository name like "gmt"
   assetPrefix: isProd ? '/gmt/' : '',
   basePath: isProd ? '/gmt' : '',
+
   images: {
+    // Required for static HTML export to avoid issues with Next.js image optimization
     unoptimized: true,
   },
-  experimental: {
-    esmExternals: true, // Enable ES Module compatibility
-  },
-  output: "export",
-  assetPrefix: "./", // Ensures relative paths
-  trailingSlash: true, // Adds trailing slash for compatibility
+
+  trailingSlash: true, // Ensures trailing slashes for all paths, improving compatibility
+  output: 'export', // Configures Next.js for static site export
 };
